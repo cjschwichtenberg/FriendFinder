db546e34b308880316037d7c11c02c400899d766
Syd Vicious <cjschwichtenberg@gmail.com>
2018-08-11 10:02:56 -0400

    updating apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,52 @@
-// Connecting to friends data file
-// const friends = require('..data/friends.js');
-var bodyParser = require("body-parser");
-var jsonParser = bodyParser.json();
-
-// console.log connection:
-// console.log('Connected tofriends.js File');
+// Connect to friends.js file
+var friends = require('..data/friends.js');
+console.log('Friends: ' + friends);
 
 module.exports = function(app) {
-    console.log("here");
-//     // Creating Express route to home.html
-//     app.get("*", function(request, response){
-//         response.sendFile(path.join(__dirname, "/../public/home.html"))
-//     })
+
+    app.get('/data/friends', function(req, res){
+        res.json(friends);
+    });
+
+    app.post("/data/friends"), function(req,res){
+        var addPerson = req.body;
+        console.log(addPerson),
+
+        var bestMatch = {
+            name = "",
+            photo = "",
+            friendDifference = 1000
+        };
+
+        var userData = req.body;
+        var userName = userData.name;
+        var userPhoto = userData.photo;
+        var userScores = userData.scores;
+        console.log (userData);
+        console.log (userName);
+        console.log(userPhoto);
+        console.log(userScores);
+
+        var totalDifference = 0;
+        
+        for (var i = 0; i < friends[i].length; i++) {
+            console.log(friends[i].name);
+            totalDifference = 0;
+            
+            for(var j = 0; j <friends[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                if (totalDifference <= bestMatch.friendDifference){
+					bestMatch.name = friends[i].name;
+					bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
+            }
+        }
+        
+        friends.push(userData);
+        
+        res.json(bestMatch);
+    });
 }
 
