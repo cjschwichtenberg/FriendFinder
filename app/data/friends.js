b15769c2a69a0b7becb120d1121605460fb17ac1
Syd Vicious <cjschwichtenberg@gmail.com>
2018-08-13 12:34:42 -0400

    changed friends.js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,21 @@
 var friends = [
     {
-       "name":"Ahmed",
-       "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+       "name":"Carly J Schwichtenberg",
+       "photo":"https://scontent-iad3-1.xx.fbcdn.net/v/t31.0-8/26952291_10104910558907636_1527996830547810898_o.jpg?_nc_cat=0&oh=92522435163ce08a1a6b35c519902d73&oe=5C0E9DCF",
        "scores":[5,1,4,4,5,1,2,5,4,1]
     },
+
+    {
+        "name":"Patrick Lehmann",
+        "photo": "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/37889951_120015918925438_2049558183197802496_o.jpg?_nc_cat=0&oh=13a3b4e1106c3a7595378262da108d11&oe=5C06C061",
+        "scores":[5,2,3,3,5,4,2,3,4,5]
+    },
+
+    {
+        "name":"Mason Stigall",
+        "photo":"https://media.licdn.com/dms/image/C4D03AQGtRJSbzTcsyA/profile-displayphoto-shrink_100_100/0?e=1539820800&v=beta&t=oCkwlv69qbm6MGL2uKiULSECtlq9IaDowH26c_XbUCY",
+        "scores":[5,5,3,4,5,1,2,2,2,5]
+    }
 ]
 
 module.exports = friends;
