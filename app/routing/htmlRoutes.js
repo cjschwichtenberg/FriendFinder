ab703631a89c2a9bedab0b64ad1de229c2119c84
Syd Vicious <cjschwichtenberg@gmail.com>
2018-08-13 12:17:33 -0400

    updated apiRoutes, home.html, survey.html and friends.js files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,15 +4,13 @@ var path = require('path');
 // Route to survey.html or default to home.html
 module.exports = function(app) {
     // Use Express to route/ perform GET request to survey.html
-    app.get("*", function(req, res){
-        res.sendFile(path.join(__dirname, "/../public/survey.html"))
-        console.log(res);
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
     // if survey.html is not found, default to home.html
-    app.use("*", function(req, res){
-        res.sendFile(path.join(__dirname, "/../public/home.html"))
-        console.log(res);
+    app.use("/", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 }
 
